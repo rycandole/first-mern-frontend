fc9181bade6981312673386ea55dad5013bea270
Ryan C <ry.candole@gmail.com>
2023-02-08 14:27:38 +0800

    add loader

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 
 // components
@@ -7,6 +7,7 @@ import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
   const { workouts, dispatch } = useWorkoutsContext()
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     const fetchWorkouts = async () => {
@@ -14,6 +15,7 @@ const Home = () => {
       const json = await response.json()
 
       if (response.ok) {
+        setLoading(true)
         dispatch({ type: 'SET_WORKOUTS', payload: json })
       }
     }
@@ -27,6 +29,9 @@ const Home = () => {
         {workouts && workouts.map(workout => (
           <WorkoutDetails workout={workout} key={workout._id} />
         ))}
+        {loading ? '' : <div className="loader__container"><svg viewBox="25 25 50 50">
+          <circle r="20" cy="50" cx="50"></circle>
+        </svg></div>}
       </div>
       <WorkoutForm />
     </div>
